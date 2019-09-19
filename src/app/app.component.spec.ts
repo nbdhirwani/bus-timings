ac1c9613e84aa1ecf27904d53930316bdc8ec267
Nirav Dhirwani <niravdhirwani@nbnco.com.au>
2019-09-19 14:48:15 +1000

    Changed app component to display new title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'bus-timings'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('bus-timings');
+    expect(app.title).toEqual('Bus Reports');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('bus-timings app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Bus Reports');
   });
 });
